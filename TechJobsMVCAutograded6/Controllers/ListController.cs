30fb4e7a8bce904bf29dc675b5d215897d25fd9c
Devon Lewis <devonlewis_23@yahoo.com>
2023-04-13 14:23:48 -0500

    assignment 3- completed

--- TechJobsMVCAutograded6/Controllers/ListController.cs
+++ TechJobsMVCAutograded6/Controllers/ListController.cs
@@ -44,6 +44,21 @@ public class ListController : Controller
     // TODO #2 - Complete the Jobs action method
     public IActionResult Jobs(string column, string value)
     {
+        List<Job> jobs = new List<Job>();
+        if(column.ToLower().Equals("View All"))
+        {
+            jobs = JobData.FindAll();
+            ViewBag.Title = "View All";
+        }
+        else
+        {
+            jobs = JobData.FindByColumnAndValue(column, value);
+            ViewBag.Title = $"{ColumnChoices[column]}: {value}";
+
+        }
+
+        ViewBag.jobs = jobs;
+
         return View();
     }
 }
